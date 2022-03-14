c94718b206e8caedff3dec38622e4b35591c6054
Muhammad Yousaf Saddique <yousafsaddique523@gmail.com>
2022-03-14 14:10:14 +0500

    Sign In Google

--- food_app/lib/auth/sign_in.dart
+++ food_app/lib/auth/sign_in.dart
@@ -1,10 +1,42 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/button_list.dart';
 import 'package:flutter_signin_button/button_view.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-class SignIn extends StatelessWidget {
+class SignIn extends StatefulWidget {
+  @override
+  State<SignIn> createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  Future<User?> _googleSignUp() async {
+    try {
+      final GoogleSignIn _googleSignIn = GoogleSignIn(
+        scopes: ['email'],
+      );
+      final FirebaseAuth _auth = FirebaseAuth.instance;
+
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      final User? user = (await _auth.signInWithCredential(credential)).user;
+      // print("signed in " + user.displayName);
+
+      return user;
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +50,7 @@ class SignIn extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
+              SizedBox(
                 height: 400,
                 width: double.infinity,
                 child: Column(
@@ -41,7 +73,17 @@ class SignIn extends StatelessWidget {
                         SignInButton(
                           Buttons.Google,
                           text: "Sign in with Google",
-                          onPressed: () {},
+                          onPressed: () {
+                            _googleSignUp().then(
+                              (value) => Navigator.of(context).pushReplacement(
+                                MaterialPageRoute(
+                                  builder: (context) => Scaffold(
+                                    body: Text("Hello"),
+                                  ),
+                                ),
+                              ),
+                            );
+                          },
                         ),
                         SignInButton(
                           Buttons.Facebook,
--- food_app/lib/main.dart
+++ food_app/lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:food_app/auth/sign_in.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
